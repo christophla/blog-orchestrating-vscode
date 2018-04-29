753d4bc3ed9134434bc7600f651eb101cdf6d355
Christopher Town <christopher@christophertown.com>
2018-04-29 08:37:32 -0500

    Commit Part 3

---
+++ scripts/project-tasks.ps1
@@ -0,0 +1,104 @@
+<#
+.SYNOPSIS
+	Orchestrates docker containers.
+.PARAMETER Clean
+	Removes the image and kills all containers based on that image.
+.PARAMETER Compose
+	Builds and runs a Docker image.
+.PARAMETER Environment
+	The environment to compose, defaults to development (docker-compose.yml)
+.EXAMPLE
+	C:\PS> .\project-tasks.ps1 -Build -Environment Debug -Quiet 
+#>
+
+# #############################################################################
+# Params
+#
+[CmdletBinding(PositionalBinding = $false)]
+Param(
+    [switch]$Clean,
+    [switch]$Compose,
+    [ValidateNotNullOrEmpty()]
+    [String]$Environment = "development"
+)
+
+
+# #############################################################################
+# Settings
+#
+$Environment = $Environment.ToLowerInvariant()
+
+
+# #############################################################################
+# Kills all running containers of an image
+#
+Function CleanAll () {
+
+    Write-Host "++++++++++++++++++++++++++++++++++++++++++++++++" -ForegroundColor "Green"
+    Write-Host "+ Cleaning projects and docker images           " -ForegroundColor "Green"
+    Write-Host "++++++++++++++++++++++++++++++++++++++++++++++++" -ForegroundColor "Green"
+
+    dotnet clean
+    
+    $composeFileName = "docker-compose.yml"
+    If ($Environment -ne "development") {
+        $composeFileName = "docker-compose.$Environment.yml"
+    }
+
+    If (Test-Path $composeFileName) {
+        docker-compose -f "$composeFileName" -p $ProjectName down --rmi all
+
+        $danglingImages = $(docker images -q --filter 'dangling=true')
+        If (-not [String]::IsNullOrWhiteSpace($danglingImages)) {
+            docker rmi -f $danglingImages
+        }
+        Write-Host "Removed docker images" -ForegroundColor "Yellow"
+    }
+    else {
+        Write-Error -Message "Environment '$Environment' is not a valid parameter. File '$composeFileName' does not exist." -Category InvalidArgument
+    }
+}
+
+
+# #############################################################################
+# Compose docker images
+#
+Function Compose () {
+
+    Write-Host "++++++++++++++++++++++++++++++++++++++++++++++++" -ForegroundColor "Green"
+    Write-Host "+ Composing docker images                       " -ForegroundColor "Green"
+    Write-Host "++++++++++++++++++++++++++++++++++++++++++++++++" -ForegroundColor "Green"
+
+    $composeFileName = "docker-compose.yml"
+    If ($Environment -ne "development") {
+        $composeFileName = "docker-compose.$Environment.yml"
+    }
+
+    If (Test-Path $composeFileName) {
+
+        Write-Host "Building the image..." -ForegroundColor "Yellow"
+        docker-compose -f "$composeFileName" build
+
+        Write-Host "Creating the container..." -ForegroundColor "Yellow"
+        docker-compose -f $composeFileName kill
+        docker-compose -f $composeFileName up -d
+    }
+    else {
+        Write-Error -Message "Environment '$Environment' is not a valid parameter. File '$composeFileName' does not exist." -Category InvalidArgument
+    }
+}
+
+
+# #############################################################################
+# Switch arguments
+#
+If ($Clean) {
+    CleanAll
+}
+ElseIf ($ComposeForDebug) {
+    $env:REMOTE_DEBUGGING = "enabled"
+    Compose
+}
+
+
+# #############################################################################
